5573a6410e2b4abefc66d75f16716c7210ec5730
Samer Buna <samer.buna@gmail.com>
2016-09-29 10:41:27 -0700

    Handling data edge cases

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,20 +4,28 @@ const Book = ({ book, handleDelete }) => {
   const handleClick = (event) => {
     event.preventDefault();
     handleDelete(book.id);
-  }
+  };
+
+  const formatPrice = priceInCents => {
+    if (priceInCents === undefined) {
+      return '';
+    }
+    return `$ ${(priceInCents/100).toFixed(2)}`;
+  };
+
   return (
     <li className="book">
       <div className="title">
         {book.title}
       </div>
       <div className="price">
-        {book.price}
+        {formatPrice(book.price)}
       </div>
       <a href="#" className="delete" onClick={handleClick}>
         delete
       </a>
     </li>
   );
-}
+};
 
 export default Book;
