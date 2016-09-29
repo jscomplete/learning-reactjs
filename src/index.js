bc0c5bbbc785960dd5e6240b1d9aa3b3b032606c
Samer Buna <samer.buna@gmail.com>
2016-09-28 20:36:39 -0700

    Building the presentational components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import { data } from './data';
+
+const Book = ({ book }) => (
+  <li>
+    {book.title} -- {book.price}
+  </li>
+);
+
+const BookList = (props) => (
+  <ul>
+    {props.books.map(book => {
+      return (
+        <Book key={book.id} book={book} />
+      );
+    })}
+  </ul>
+);
 
 ReactDOM.render(
-  <App />,
+  <BookList books={data.books} />,
   document.getElementById('root')
 );
